51936ad7f429831668fdc7692e542b73296407e6
TomMorane <moranetom@gmail.com>
2022-10-05 11:46:45 +0200

    step3

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let john = "hello world";
+function getComputerChoice() {
+  let computerRandomChoices = ["rock", "paper", "scissors"];
+  let getRandomChoices = Math.floor(
+    Math.random() * computerRandomChoices.length
+  );
 
-console.log(john);
+  return computerRandomChoices[getRandomChoices];
+}
+
+console.log(getComputerChoice());
